7b2293cb9beb4031abd2faa2928c5ef554c97c19
Martin Stofko <martinstofko114@gmail.com>
2018-08-03 18:27:46 -0700

    Added ability to save funny sentences.

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,33 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Word Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Word Game'),
-        ),
-        body: Center(child: RandomSentences()),
-      ),
-    );
+    return MaterialApp(title: 'Word Game', home: RandomSentences());
   }
 }
 
 class RandomSentences extends StatefulWidget {
   @override
-  State<RandomSentences> createState() => new _RandomSentences();
+  State<RandomSentences> createState() => _RandomSentences();
 }
 
 class _RandomSentences extends State<RandomSentences> {
   final _sentences = <String>[];
+  final _funnies = Set<String>();
 
   @override
   Widget build(BuildContext context) {
-    return _buildSentencesListView();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Game'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushFunnies,
+          ),
+        ],
+      ),
+      body: _buildSentencesListView(),
+    );
   }
 
   String _getSentence() {
@@ -38,19 +42,34 @@ class _RandomSentences extends State<RandomSentences> {
   }
 
   Widget _buildListTile(String sentence) {
+    final isFunny = _funnies.contains(sentence);
+
     return ListTile(
       title: Text(sentence),
+      trailing: Icon(
+        Icons.thumb_up,
+        color: isFunny ? Colors.blue : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (!isFunny) {
+            _funnies.add(sentence);
+          } else {
+            _funnies.remove(sentence);
+          }
+        });
+      },
     );
   }
 
   Widget _buildSentencesListView() {
     // max number of sentences
-    final _maxNum = 100; 
-    
+    final maxNum = 100;
+
     // remove itemCount and maxNum check in itemBuilder to make the list view infinitely scrolling
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemCount: _maxNum,
+      itemCount: maxNum,
       itemBuilder: (context, i) {
         // add divider between each row
         if (i.isOdd)
@@ -62,7 +81,7 @@ class _RandomSentences extends State<RandomSentences> {
         // dividing by 2 since we're adding divider between each sentence of the list view
         final index = i ~/ 2;
         // checks for end of list view
-        if (index >= _sentences.length && _sentences.length < _maxNum) {
+        if (index >= _sentences.length && _sentences.length < maxNum) {
           // generate more sentences
           for (int x = 0; x < 10; x++) {
             _sentences.add(_getSentence());
@@ -70,6 +89,31 @@ class _RandomSentences extends State<RandomSentences> {
         }
         return _buildListTile(_sentences[index]);
       },
+    );
+  }
+
+  void _pushFunnies() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        final listTiles = _funnies.map((sentence) {
+          return ListTile(
+            title: Text(sentence),
+          );
+        });
+
+        final dividedList =
+            ListTile.divideTiles(context: context, tiles: listTiles).toList();
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Funny Sentences'),
+          ),
+          body: ListView(
+            padding: EdgeInsets.all(16.0),
+            children: dividedList,
+          ),
+        );
+      }),
     );
   }
 }
